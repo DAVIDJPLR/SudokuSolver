3ba00c579fc2c044eab5156ce11720145ff1f6b3
DavidJPLeRoux <djpleroux@gmail.com>
2024-03-18 13:24:27 -0400

    Should work

--- BacktrackSolver.py
+++ BacktrackSolver.py
@@ -1,7 +1,33 @@
+import sys
+from GenerateSudoku import *
+from BacktrackSolver import *
 from Sudoku.Generator import *
-def backtrack(board: Board):
+
+def runBacktrack(difficultylevel, verbosity: int):
+    unsolved: Board = generateSudoku(difficultylevel)
+
+    results = backtrack(unsolved, verbosity)
+    solved = results[0]
+    visitedNodes = results[1]
+    openVals = results[2]
+
+    totalNodesExplored = results[1]
+    ratio = totalNodesExplored/openVals
+    totalNodes_Opens = ratio
+
+    if verbosity == 2:
+        print("=====================================")
+        print(str(totalNodesExplored) + " nodes were visited")
+        print(str(openVals) + " open squares in the starting state")
+        print(str(totalNodes_Opens) + " = (# nodes visited)/(# of open squares in starting state)")
+        print("=====================================")
+
+def backtrack(board: Board, verbosity: int):
     numOpenVals = len(board.get_unused_cells())
     nodesVisited = [0]
+
+    print("The original unsolved sudoku puzzle is: \r\r\n{0}".format(board) + "\n")
+
     def solve():
         openCells = board.get_unused_cells()
         if len(openCells) == 0:
@@ -11,11 +37,21 @@ def backtrack(board: Board):
             if valid(board, cell, number):
                 nodesVisited[0] += 1
                 cell.value = number
+                
+                if verbosity == 3:
+                    print("Changing value of (" + str(cell.row) + "," + str(cell.col) + ") to " + str(number) + ": \r\r\n{0}".format(board) + "\n")
+
                 if solve():
                     return True
                 cell.value = 0
+
+                if verbosity == 3:
+                    print("Backtracking and changing value of (" + str(cell.row) + "," + str(cell.col) + ") back to " + str(0) + ": \r\r\n{0}".format(board) + "\n")
         return False
-    solve()  
+    solve() 
+
+    print("The solved sudoku puzzule is: \r\r\n{0}".format(board) + "\n")
+
     return [board, nodesVisited[0], numOpenVals]
 
 def valid(board: Board, cell: Cell, number: int):
--- main.py
+++ main.py
@@ -2,99 +2,118 @@ import sys
 from GenerateSudoku import *
 from BacktrackSolver import *
 
+try:
+    algo = sys.argv[1]
+    difficulty = sys.argv[2]
+    verbosity = int(sys.argv[3])
 
-print("easy -- results")
-totalNodesExplored = 0
-totalNodes_Opens = 0
+    if (algo == "backtrack"):
+        runBacktrack(difficulty, verbosity)
+    elif (algo == "exactcover"):
+        print("exactcover")
+    else:
+        print("Incorrect arguments, usage example: py main.py algorithm difficulty verbosity")
+        print("algorithm usages: backtrack, exactcover")
+        print("difficulty usages: easy, medium, hard, extreme")
+        print("verbosity usages: 1, 2, 3")
+except:
+    print("Incorrect arguments, usage example: py main.py algorithm difficulty verbosity")
+    print("algorithm usages: backtrack, exactcover")
+    print("difficulty usages: easy, medium, hard, extreme")
+    print("verbosity usages: 1, 2, 3")
 
-for i in range(8):
-    unsolved: Board = generateSudoku('easy')
+# print("easy -- results")
+# totalNodesExplored = 0
+# totalNodes_Opens = 0
 
-    results = backtrack(unsolved)
-    solved = results[0]
-    visitedNodes = results[1]
-    openVals = results[2]
+# for i in range(8):
+#     unsolved: Board = generateSudoku('easy')
 
-    totalNodesExplored += results[1]
-    ratio = results[1]/results[2]
-    totalNodes_Opens += ratio
+#     results = backtrack(unsolved)
+#     solved = results[0]
+#     visitedNodes = results[1]
+#     openVals = results[2]
 
-avgNodesVisited = totalNodesExplored/7
-avgRatio = totalNodes_Opens/7
+#     totalNodesExplored += results[1]
+#     ratio = results[1]/results[2]
+#     totalNodes_Opens += ratio
 
-print(str(avgNodesVisited) + " nodes visited on average")
-print(str(avgRatio) + " nodes visited/open nodes on average")
-print("=====================================")
+# avgNodesVisited = totalNodesExplored/7
+# avgRatio = totalNodes_Opens/7
 
-
-print("medium -- results")
-totalNodesExplored = 0
-totalNodes_Opens = 0
-
-for i in range(8):
-    unsolved: Board = generateSudoku('medium')
-
-    results = backtrack(unsolved)
-    solved = results[0]
-    visitedNodes = results[1]
-    openVals = results[2]
-
-    totalNodesExplored += results[1]
-    ratio = results[1]/results[2]
-    totalNodes_Opens += ratio
-
-avgNodesVisited = totalNodesExplored/7
-avgRatio = totalNodes_Opens/7
-
-print(str(avgNodesVisited) + " nodes visited on average")
-print(str(avgRatio) + " nodes visited/open nodes on average")
-print("=====================================")
+# print(str(avgNodesVisited) + " nodes visited on average")
+# print(str(avgRatio) + " nodes visited/open nodes on average")
+# print("=====================================")
 
 
-print("hard -- results")
-totalNodesExplored = 0
-totalNodes_Opens = 0
+# print("medium -- results")
+# totalNodesExplored = 0
+# totalNodes_Opens = 0
 
-for i in range(8):
-    unsolved: Board = generateSudoku('hard')
+# for i in range(8):
+#     unsolved: Board = generateSudoku('medium')
 
-    results = backtrack(unsolved)
-    solved = results[0]
-    visitedNodes = results[1]
-    openVals = results[2]
+#     results = backtrack(unsolved)
+#     solved = results[0]
+#     visitedNodes = results[1]
+#     openVals = results[2]
 
-    totalNodesExplored += results[1]
-    ratio = results[1]/results[2]
-    totalNodes_Opens += ratio
+#     totalNodesExplored += results[1]
+#     ratio = results[1]/results[2]
+#     totalNodes_Opens += ratio
 
-avgNodesVisited = totalNodesExplored/7
-avgRatio = totalNodes_Opens/7
+# avgNodesVisited = totalNodesExplored/7
+# avgRatio = totalNodes_Opens/7
 
-print(str(avgNodesVisited) + " nodes visited on average")
-print(str(avgRatio) + " nodes visited/open nodes on average")
-print("=====================================")
+# print(str(avgNodesVisited) + " nodes visited on average")
+# print(str(avgRatio) + " nodes visited/open nodes on average")
+# print("=====================================")
 
 
-print("extreme -- results")
-totalNodesExplored = 0
-totalNodes_Opens = 0
+# print("hard -- results")
+# totalNodesExplored = 0
+# totalNodes_Opens = 0
 
-for i in range(8):
-    unsolved: Board = generateSudoku('extreme')
+# for i in range(8):
+#     unsolved: Board = generateSudoku('hard')
 
-    results = backtrack(unsolved)
-    solved = results[0]
-    visitedNodes = results[1]
-    openVals = results[2]
+#     results = backtrack(unsolved)
+#     solved = results[0]
+#     visitedNodes = results[1]
+#     openVals = results[2]
 
-    totalNodesExplored += results[1]
-    ratio = results[1]/results[2]
-    totalNodes_Opens += ratio
+#     totalNodesExplored += results[1]
+#     ratio = results[1]/results[2]
+#     totalNodes_Opens += ratio
 
-avgNodesVisited = totalNodesExplored/7
-avgRatio = totalNodes_Opens/7
+# avgNodesVisited = totalNodesExplored/7
+# avgRatio = totalNodes_Opens/7
 
-print(str(avgNodesVisited) + " nodes visited on average")
-print(str(avgRatio) + " nodes visited/open nodes on average")
-print("=====================================")
+# print(str(avgNodesVisited) + " nodes visited on average")
+# print(str(avgRatio) + " nodes visited/open nodes on average")
+# print("=====================================")
+
+
+# print("extreme -- results")
+# totalNodesExplored = 0
+# totalNodes_Opens = 0
+
+# for i in range(8):
+#     unsolved: Board = generateSudoku('extreme')
+
+#     results = backtrack(unsolved)
+#     solved = results[0]
+#     visitedNodes = results[1]
+#     openVals = results[2]
+
+#     totalNodesExplored += results[1]
+#     ratio = results[1]/results[2]
+#     totalNodes_Opens += ratio
+
+# avgNodesVisited = totalNodesExplored/7
+# avgRatio = totalNodes_Opens/7
+
+# print(str(avgNodesVisited) + " nodes visited on average")
+# print(str(avgRatio) + " nodes visited/open nodes on average")
+# print("=====================================")
 
